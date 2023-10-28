122f22ab00f6213215dd3ea1d918c0d1087c3648
Prashant Khunt <prashantkhunt1999@gmail.com>
2023-10-28 15:55:46 +0530

    Added: auth module

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,13 +1,12 @@
-import { Button } from '@/components/ui/button'
-import Image from 'next/image'
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return(
     <div>
-      <p className='text-3xl font-medium text-sky-700' >Hello World</p>
-      <Button variant={'destructive'}>
-        Click Me
-      </Button>
+      <h1>Dashboard</h1>
+      <UserButton 
+        afterSignOutUrl="/"
+      />
     </div>
   )
 }
